e9df8d717d3a8bd6c3382eeb78cf152ba3bfc3f3
semenov.v <semenov.v@websterstd.com>
2022-09-19 18:27:34 +0300

    do submenu

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -13,10 +13,28 @@
   };
 
   headerMenuDropDown.addEventListener('click', () => toggleActiveElem(headerContactList));
-
   headerBurger.addEventListener('click', () => toggleActiveElem(header, headerNav));
 
 
+
+  // copy text
+  function copyToClipboard(str) {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  };
+
+  const strList = header.querySelectorAll('.header-contact__link');
+
+  strList.forEach(str => {
+    const copied = str.innerText;
+    const buttonCopy = str.parentNode.querySelector('.header-contact__button-copy');
+    buttonCopy.addEventListener('click', () => copyToClipboard(copied) );
+  });
 
 
 
