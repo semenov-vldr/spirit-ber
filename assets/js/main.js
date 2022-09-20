bb2d865da426237944c6ed9759a1234273b8af4d
semenov.v <semenov.v@websterstd.com>
2022-09-20 09:00:11 +0300

    Updates

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+
+{
+
+  const header = document.querySelector('.header');
+  const headerMenuDropDown = header.querySelector('.header-contact__menu-tablet');
+  const headerContactList = header.querySelector('.header-contact__list');
+
+  const headerNav = header.querySelector('.header-nav');
+  const headerBurger = header.querySelector('.header__burger');
+
+  function toggleActiveElem(...items) {
+    items.forEach( item => item.classList.toggle('js-active') );
+  };
+
+  headerMenuDropDown.addEventListener('click', () => toggleActiveElem(headerContactList));
+  headerBurger.addEventListener('click', () => toggleActiveElem(header, headerNav));
+
+
+
+  // copy text
+  function copyToClipboard(str) {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  };
+
+  const strList = header.querySelectorAll('.header-contact__link');
+
+  strList.forEach(str => {
+    const copied = str.innerText;
+    const buttonCopy = str.parentNode.querySelector('.header-contact__button-copy');
+    buttonCopy.addEventListener('click', () => copyToClipboard(copied) );
+  });
+
+
+
+}
