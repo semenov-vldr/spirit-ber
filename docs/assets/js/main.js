23827224e88964a3a0529e2c14f48d379151128c
semenov.v <semenov.v@websterstd.com>
2022-09-26 18:18:36 +0300

    add hero-service

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,92 +1,6 @@
 
 {
 
-  const heroIndex = document.querySelector('.hero-index');
-
-  if (heroIndex) {
-
-    const prev = heroIndex.querySelector('.hero-slider-nav__item--prev');
-    const next = heroIndex.querySelector('.hero-slider-nav__item--next');
-    const slides = heroIndex.querySelectorAll('.hero-slide');
-    const dots = heroIndex.querySelectorAll('.hero-slider-pagination__item');
-
-    const addClassActive = (item) => item.classList.add('js-hero-active');
-    const removeClassActive = (item) => item.classList.remove('js-hero-active');
-
-    dots.forEach(dot => {
-      addClassActive(dots[0]);
-      addClassActive (slides[0]);
-      dot.addEventListener('click', () => {
-        dots.forEach(dot => removeClassActive(dot) );
-        slides.forEach(slide => removeClassActive(slide) );
-        addClassActive(dot);
-        const numberDots = dot.getAttribute('data-pagination');
-        slides.forEach(slide => {
-          const numberSlide = slide.getAttribute('data-slide');
-          if (numberDots === numberSlide) addClassActive(slide);
-        });
-      });
-    });
-
-
-    let index = 0;
-
-    const activeSlide = (num) => {
-      slides.forEach(slide => removeClassActive(slide) );
-      addClassActive(slides[num]);
-    };
-
-    const activeSDot = (num) => {
-      dots.forEach(dot => removeClassActive(dot) );
-      addClassActive(dots[num]);
-    };
-
-    const currentSlide = (idx) => {
-      activeSlide(idx);
-      activeSDot(idx)
-    };
-
-    const nextSlide = () => {
-      if (index === slides.length - 1) {
-        index = 0;
-        currentSlide(index);
-      } else {
-        index++;
-        currentSlide(index);
-      }
-    }
-
-    const prevSlide = () => {
-      if (index === 0) {
-        index = slides.length - 1;
-        currentSlide(index);
-      } else {
-        index--;
-        currentSlide(index);
-      }
-    };
-
-    next.addEventListener('click', nextSlide);
-    prev.addEventListener('click', prevSlide);
-
-    dots.forEach((dot, indexDot) => {
-      dot.addEventListener('click', () => {
-        index = indexDot;
-        currentSlide(index);
-      });
-    });
-
-
-
-
-
-  }
-
-
-}
-
-{
-
   const header = document.querySelector('.header');
   const headerMenuDropDown = header.querySelector('.header-contact__menu-tablet');
   const headerContactList = header.querySelector('.header-contact__list');
@@ -197,3 +111,137 @@
 
 
     }
+
+{
+
+  const heroIndex = document.querySelector('.hero-index');
+
+  if (heroIndex) {
+
+    const prev = heroIndex.querySelector('.arrow-nav__prev');
+    const next = heroIndex.querySelector('.arrow-nav__next');
+    const slides = heroIndex.querySelectorAll('.hero-slide');
+    const dots = heroIndex.querySelectorAll('.hero-slider-pagination__item');
+
+    const addClassActive = (item) => item.classList.add('js-hero-active');
+    const removeClassActive = (item) => item.classList.remove('js-hero-active');
+
+    dots.forEach(dot => {
+      addClassActive(dots[1]);
+      addClassActive (slides[1]);
+      dot.addEventListener('click', () => {
+        dots.forEach(dot => removeClassActive(dot) );
+        slides.forEach(slide => removeClassActive(slide) );
+        addClassActive(dot);
+        const numberDots = dot.getAttribute('data-pagination');
+        slides.forEach(slide => {
+          const numberSlide = slide.getAttribute('data-slide');
+          if (numberDots === numberSlide) addClassActive(slide);
+        });
+      });
+    });
+
+
+    let index = 0;
+
+    const activeSlide = (num) => {
+      slides.forEach(slide => removeClassActive(slide) );
+      addClassActive(slides[num]);
+    };
+
+    const activeSDot = (num) => {
+      dots.forEach(dot => removeClassActive(dot) );
+      addClassActive(dots[num]);
+    };
+
+    const currentSlide = (idx) => {
+      activeSlide(idx);
+      activeSDot(idx)
+    };
+
+    const nextSlide = () => {
+      if (index === slides.length - 1) {
+        index = 0;
+        currentSlide(index);
+      } else {
+        index++;
+        currentSlide(index);
+      }
+    }
+
+    const prevSlide = () => {
+      if (index === 0) {
+        index = slides.length - 1;
+        currentSlide(index);
+      } else {
+        index--;
+        currentSlide(index);
+      }
+    };
+
+    next.addEventListener('click', nextSlide);
+    prev.addEventListener('click', prevSlide);
+
+    dots.forEach((dot, indexDot) => {
+      dot.addEventListener('click', () => {
+        index = indexDot;
+        currentSlide(index);
+      });
+    });
+
+
+
+
+
+  }
+
+
+}
+
+{
+  const heroService = document.querySelector('.hero-service');
+  const tabsBtn = heroService.querySelectorAll('.hero-service-nav__name');
+  const tabsContent = heroService.querySelectorAll('.hero-service__item');
+
+  const classActive = 'js-service-active';
+
+  if (heroService) {
+
+    //const addClassActive = (item) => item.classList.add(classActive);
+    const removeClassActive = (item) => item.classList.remove(classActive);
+
+    heroService.addEventListener('click', (evt) => {
+      if (evt.target.classList.contains('hero-service-nav__name') || evt.target.classList.contains('hero-service-nav__number')) {
+        const tabsPath = evt.target.parentNode.dataset.tab;
+        tabsBtn.forEach(tab => removeClassActive(tab.parentNode) );
+        heroService.querySelector(`[data-tab="${tabsPath}"]`).classList.add(classActive);
+        tabsHandler(tabsPath);
+      }
+    });
+
+    function tabsHandler (path) {
+      tabsContent.forEach(contentItem => {
+        removeClassActive(contentItem)
+        heroService.querySelector(`[data-content="${path}"]`).classList.add(classActive);
+      })
+    };
+
+
+
+    const mobileWidth = window.matchMedia('(max-width: 1100px)').matches;
+    if (mobileWidth) {
+      const heroServiceButtons = heroService.querySelectorAll('.button');
+      heroServiceButtons.forEach(btn => {
+        btn.classList.remove('button--bg-accept');
+        btn.classList.add('button--bg-grey')
+      })
+    } else {
+      btn.classList.remove('button--bg-grey')
+      btn.classList.add('button--bg-accept');
+    }
+
+
+  }
+
+
+}
---
+++ src/components/hero/hero-service/hero-service.js
@@ -0,0 +1,47 @@
+{
+  const heroService = document.querySelector('.hero-service');
+  const tabsBtn = heroService.querySelectorAll('.hero-service-nav__name');
+  const tabsContent = heroService.querySelectorAll('.hero-service__item');
+
+  const classActive = 'js-service-active';
+
+  if (heroService) {
+
+    //const addClassActive = (item) => item.classList.add(classActive);
+    const removeClassActive = (item) => item.classList.remove(classActive);
+
+    heroService.addEventListener('click', (evt) => {
+      if (evt.target.classList.contains('hero-service-nav__name') || evt.target.classList.contains('hero-service-nav__number')) {
+        const tabsPath = evt.target.parentNode.dataset.tab;
+        tabsBtn.forEach(tab => removeClassActive(tab.parentNode) );
+        heroService.querySelector(`[data-tab="${tabsPath}"]`).classList.add(classActive);
+        tabsHandler(tabsPath);
+      }
+    });
+
+    function tabsHandler (path) {
+      tabsContent.forEach(contentItem => {
+        removeClassActive(contentItem)
+        heroService.querySelector(`[data-content="${path}"]`).classList.add(classActive);
+      })
+    };
+
+
+
+    const mobileWidth = window.matchMedia('(max-width: 1100px)').matches;
+    if (mobileWidth) {
+      const heroServiceButtons = heroService.querySelectorAll('.button');
+      heroServiceButtons.forEach(btn => {
+        btn.classList.remove('button--bg-accept');
+        btn.classList.add('button--bg-grey')
+      })
+    } else {
+      btn.classList.remove('button--bg-grey')
+      btn.classList.add('button--bg-accept');
+    }
+
+
+  }
+
+
+}
