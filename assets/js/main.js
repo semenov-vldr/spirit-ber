7cbd4ab9fd26a1045d044b331567e82a8f6f245b
semenov-vldr <semenov-vldr@users.noreply.github.com>
2022-10-09 18:47:45 +0000

    deploy: 599428825a0226ac2b49fafee841e44d8a975e53

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,6 +66,91 @@
 
 }
 
+const phoneInputs = document.querySelectorAll('input[data-tel-input]');
+
+const getInputNumbersValue = (input) => {
+  return input.value.replace(/\D/g,"");
+};
+
+const onPhoneInput = (evt) => {
+  const input = evt.target;
+  let inputNumbersValue = getInputNumbersValue(input);
+  let formattedInputValue = "";
+  let selectionStart = input.selectionStart;
+
+  if ( !inputNumbersValue ) input.value = "";
+
+
+  if ( input.value.length != selectionStart ) {
+    if ( evt.data && /\D/g.test(evt.data) ) {
+      input.value = formattedInputValue;
+    }
+    return;
+  }
+
+  if ( ["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1 ) {
+    // Российские номера
+    if (inputNumbersValue[0] == "9") inputNumbersValue = "7" + inputNumbersValue;
+    let firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";
+    formattedInputValue = firstSymbols + " ";
+
+    if (inputNumbersValue[0] == "8") {
+      phoneInputs[0].setAttribute("pattern", ".{17,}");
+      console.log(phoneInputs[0].getAttribute("pattern"));
+    }
+
+    if (inputNumbersValue.length > 1) {
+      formattedInputValue += "(" + inputNumbersValue.slice(1, 4);
+    }
+
+    if (inputNumbersValue.length >= 5) {
+      formattedInputValue += ") " + inputNumbersValue.slice(4, 7);
+    }
+
+    if (inputNumbersValue.length >= 8) {
+      formattedInputValue += "-" + inputNumbersValue.slice(7, 9);
+    }
+
+    if (inputNumbersValue.length >= 10) {
+      formattedInputValue += "-" + inputNumbersValue.slice(9, 11);
+    }
+
+// Не российские номера
+  } else formattedInputValue = "+" + inputNumbersValue;
+
+  input.value = formattedInputValue;
+};
+
+// Стирание первого символа
+const onPhoneKeyDown = (evt) => {
+  const input = evt.target;
+  if (evt.keyCode == 8 && getInputNumbersValue(input).length == 1) {
+    input.value = "";
+  }
+};
+
+// Вставка цифр в любое место
+const onPhonePaste = (evt) => {
+  const pasted = evt.clipboardData || window.clipboardData;
+  const input = evt.target;
+  const inputNumbersValue = getInputNumbersValue(input);
+
+  if (pasted) {
+    const pastedText = pasted.getData("Text");
+    if ( /\D/g.test(pastedText) ) {
+      input.value = inputNumbersValue;
+    }
+  }
+};
+
+phoneInputs.forEach(input => {
+  input.addEventListener('input', onPhoneInput);
+  input.addEventListener("keydown", onPhoneKeyDown);
+  input.addEventListener("paste", onPhonePaste);
+});
+
+
+
 {
 
   const header = document.querySelector('.header');
@@ -246,6 +331,74 @@
 //   });
 //
 // }
+
+{
+  let swiper__thumbs = new Swiper(".portfolio-popup__swiper--thumbs", {
+    //loop: true,
+    spaceBetween: 28,
+    slidesPerView: "auto",
+    freeMode: true,
+    watchSlidesProgress: true,
+    watchSlidesVisibility: true,
+    watchOverflow: true,
+    initialSlide: 0,
+  });
+
+  let swiper__top = new Swiper(".portfolio-popup__swiper--top", {
+    loop: true,
+    slidesPerView: 1,
+    centeredSlides: true,
+    initialSlide: 0,
+    thumbs: {
+      swiper: swiper__thumbs,
+    },
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    }
+  });
+
+  //--------------------------------------
+
+  const activeClass = 'js-popup-active';
+
+  const portfolio = document.querySelector('.portfolio');
+  const portfolioItems = portfolio.querySelectorAll('.portfolio__item');
+
+
+  const addClassActive = (item) => item.classList.add(activeClass);
+  const removeClassActive = (item) => {
+    item.classList.remove(activeClass);
+    console.log('close');
+  }
+
+  portfolioItems.forEach(item => {
+    item.addEventListener('click', () => addClassActive(item));
+
+    const close = item.querySelector('.portfolio-popup__close');
+    if (close) {
+      close.addEventListener('click', () => {
+        removeClassActive(item);
+        console.log(item)
+      });
+
+    }
+
+  });
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 {
@@ -645,74 +798,6 @@
 
 
 }
-
-{
-  let swiper__thumbs = new Swiper(".portfolio-popup__swiper--thumbs", {
-    //loop: true,
-    spaceBetween: 28,
-    slidesPerView: "auto",
-    freeMode: true,
-    watchSlidesProgress: true,
-    watchSlidesVisibility: true,
-    watchOverflow: true,
-    initialSlide: 0,
-  });
-
-  let swiper__top = new Swiper(".portfolio-popup__swiper--top", {
-    loop: true,
-    slidesPerView: 1,
-    centeredSlides: true,
-    initialSlide: 0,
-    thumbs: {
-      swiper: swiper__thumbs,
-    },
-    effect: 'fade',
-    fadeEffect: {
-      crossFade: true
-    }
-  });
-
-  //--------------------------------------
-
-  const activeClass = 'js-popup-active';
-
-  const portfolio = document.querySelector('.portfolio');
-  const portfolioItems = portfolio.querySelectorAll('.portfolio__item');
-
-
-  const addClassActive = (item) => item.classList.add(activeClass);
-  const removeClassActive = (item) => {
-    item.classList.remove(activeClass);
-    console.log('close');
-  }
-
-  portfolioItems.forEach(item => {
-    item.addEventListener('click', () => addClassActive(item));
-
-    const close = item.querySelector('.portfolio-popup__close');
-    if (close) {
-      close.addEventListener('click', () => {
-        removeClassActive(item);
-        console.log(item)
-      });
-
-    }
-
-  });
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
 
 // copy text
 function copyToClipboard(str) {
