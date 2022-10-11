4f9debebf30c7c0a338945e8084073c4b463ac0e
semenov.v <semenov.v@websterstd.com>
2022-10-11 15:54:47 +0300

    text-image block

--- src/components/feedback/feedback.js
+++ src/components/feedback/feedback.js
@@ -1,90 +1,159 @@
-const phoneInputs = document.querySelectorAll('input[data-tel-input]');
+{
 
-const getInputNumbersValue = (input) => {
-  return input.value.replace(/\D/g,"");
-};
+  const phoneInputs = document.querySelectorAll('input[data-tel-input]');
 
-const onPhoneInput = (evt) => {
-  const input = evt.target;
-  let inputNumbersValue = getInputNumbersValue(input);
-  let formattedInputValue = "";
-  let selectionStart = input.selectionStart;
+  const getInputNumbersValue = (input) => {
+    return input.value.replace(/\D/g,"");
+  };
 
-  if ( !inputNumbersValue ) input.value = "";
+  const onPhoneInput = (evt) => {
+    const input = evt.target;
+    let inputNumbersValue = getInputNumbersValue(input);
+    let formattedInputValue = "";
+    let selectionStart = input.selectionStart;
+
+    if ( !inputNumbersValue ) input.value = "";
 
 
-  if ( input.value.length != selectionStart ) {
-    if ( evt.data && /\D/g.test(evt.data) ) {
-      input.value = formattedInputValue;
-    }
-    return;
-  }
-
-  if ( ["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1 ) {
-    // Российские номера
-    if (inputNumbersValue[0] == "9") inputNumbersValue = "7" + inputNumbersValue;
-    let firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";
-    formattedInputValue = firstSymbols + " ";
-
-    if (inputNumbersValue[0] == "8") {
-      phoneInputs[0].setAttribute("pattern", ".{17,}");
-      console.log(phoneInputs[0].getAttribute("pattern"));
+    if ( input.value.length != selectionStart ) {
+      if ( evt.data && /\D/g.test(evt.data) ) {
+        input.value = formattedInputValue;
+      }
+      return;
     }
 
-    if (inputNumbersValue.length > 1) {
-      formattedInputValue += "(" + inputNumbersValue.slice(1, 4);
-    }
+    if ( ["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1 ) {
+      // Российские номера
+      if (inputNumbersValue[0] == "9") inputNumbersValue = "7" + inputNumbersValue;
+      let firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";
+      formattedInputValue = firstSymbols + " ";
 
-    if (inputNumbersValue.length >= 5) {
-      formattedInputValue += ") " + inputNumbersValue.slice(4, 7);
-    }
+      if (inputNumbersValue[0] == "8") {
+        phoneInputs[0].setAttribute("pattern", ".{17,}");
+        console.log(phoneInputs[0].getAttribute("pattern"));
+      }
 
-    if (inputNumbersValue.length >= 8) {
-      formattedInputValue += "-" + inputNumbersValue.slice(7, 9);
-    }
+      if (inputNumbersValue.length > 1) {
+        formattedInputValue += "(" + inputNumbersValue.slice(1, 4);
+      }
 
-    if (inputNumbersValue.length >= 10) {
-      formattedInputValue += "-" + inputNumbersValue.slice(9, 11);
-    }
+      if (inputNumbersValue.length >= 5) {
+        formattedInputValue += ") " + inputNumbersValue.slice(4, 7);
+      }
+
+      if (inputNumbersValue.length >= 8) {
+        formattedInputValue += "-" + inputNumbersValue.slice(7, 9);
+      }
+
+      if (inputNumbersValue.length >= 10) {
+        formattedInputValue += "-" + inputNumbersValue.slice(9, 11);
+      }
 
 // Не российские номера
-  } else formattedInputValue = "+" + inputNumbersValue;
+    } else formattedInputValue = "+" + inputNumbersValue;
 
-  input.value = formattedInputValue;
-};
+    input.value = formattedInputValue;
+  };
 
 // Стирание первого символа
-const onPhoneKeyDown = (evt) => {
-  const input = evt.target;
-  if (evt.keyCode == 8 && getInputNumbersValue(input).length == 1) {
-    input.value = "";
-  }
-};
+  const onPhoneKeyDown = (evt) => {
+    const input = evt.target;
+    if (evt.keyCode == 8 && getInputNumbersValue(input).length == 1) {
+      input.value = "";
+    }
+  };
 
 // Вставка цифр в любое место
-const onPhonePaste = (evt) => {
-  const pasted = evt.clipboardData || window.clipboardData;
-  const input = evt.target;
-  const inputNumbersValue = getInputNumbersValue(input);
+  const onPhonePaste = (evt) => {
+    const pasted = evt.clipboardData || window.clipboardData;
+    const input = evt.target;
+    const inputNumbersValue = getInputNumbersValue(input);
 
-  if (pasted) {
-    const pastedText = pasted.getData("Text");
-    if ( /\D/g.test(pastedText) ) {
-      input.value = inputNumbersValue;
+    if (pasted) {
+      const pastedText = pasted.getData("Text");
+      if ( /\D/g.test(pastedText) ) {
+        input.value = inputNumbersValue;
+      }
     }
-  }
-};
+  };
 
-phoneInputs.forEach(input => {
-  input.addEventListener('input', onPhoneInput);
-  input.addEventListener("keydown", onPhoneKeyDown);
-  input.addEventListener("paste", onPhonePaste);
-});
+  phoneInputs.forEach(input => {
+    input.addEventListener('input', onPhoneInput);
+    input.addEventListener("keydown", onPhoneKeyDown);
+    input.addEventListener("paste", onPhonePaste);
+  });
 
-const forms = document.querySelectorAll('form');
 
-forms.forEach(form => {
-  const formMandatory = form.querySelector('form .feedback-form__mandatory');
 
-})
+// ---------upload file---------
 
+//   const dropZone = document.querySelector('.feedback-form-upload');
+//
+//   const events = ['dragenter', 'dragleave', 'dragover', 'drop'];
+//
+// //events.forEach(event => dropZone.addEventListener(event,  handlerFunction, false));
+//
+//   function preventDefaults (evt) {
+//     evt.preventDefault();
+//     evt.stopPropagation();
+//   };
+//
+// // Сбрасываем стандартное поведение для всех событий
+//   events.forEach(event => {
+//     dropZone.addEventListener(event, preventDefaults);
+//   });
+//
+//   function highLight (evt) {
+//     dropZone.classList.add('highlight');
+//   };
+//
+//   function unHighLight (evt) {
+//     dropZone.classList.remove('highlight');
+//   };
+//
+//   ['dragenter', 'dragover'].forEach(event => {
+//     dropZone.addEventListener(event, highLight)
+//   });
+//
+//   ['dragleave', 'drop'].forEach(event => {
+//     dropZone.addEventListener(event, unHighLight)
+//   });
+//
+//   function handleDrop (evt) {
+//     let dt = evt.dataTransfer;
+//     let files = dt.files;
+//     handleFiles(files);
+//   };
+//
+//   function handleFiles(files) {
+//     ([...files].forEach(uploadFile));
+//   };
+//
+//   function uploadFile(file) {
+//     let url = 'URL для загрузки файлов';
+//     let formData = new FormData();
+//
+//     formData.append('file', file)
+//
+//     fetch(url, {
+//       method: 'POST',
+//       body: formData
+//     })
+//       .then(() => console.log('Готово'))
+//       .catch(() => console.log('Ошибка'))
+//   }
+//
+//   dropZone.addEventListener('drop', handleDrop);
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/components/image-text/image-text.js
@@ -0,0 +1,61 @@
+
+
+
+{
+
+  const mobileWidth = window.matchMedia('(max-width: 768px)').matches;
+
+  let mySwiper;
+
+  if (mobileWidth) {
+
+    mySwiper = new Swiper('.image-text__swiper', {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      // navigation: {
+      //   nextEl: '.slider-nav__next',
+      //   prevEl: '.slider-nav__prev',
+      // },
+
+      uniqueNavElements: true,
+
+      slidesPerView: 1,
+
+      // Бесконечная прокрутка
+      loop: true,
+
+      // Откл функционала, если слайдов меньше, чем нужно
+      watchOverflow: true,
+
+      //centeredSlides: true,
+
+      // Отступ между слайдами
+      spaceBetween: 20,
+
+      // Стартовый слайд
+      initialSlide: 0,
+
+      // Брейк поинты (адаптив)
+      // Ширина экрана
+      breakpoints: {
+        // 320: {
+        //   slidesPerView: 1,
+        // },
+
+      }
+    });
+
+  }
+
+
+
+
+
+
+
+}
+
+
+
