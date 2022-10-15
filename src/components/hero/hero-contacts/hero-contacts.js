cdcc0a3ccf4ea103bbd5325a87dcf9c45f4ffbbb
Vladimir <vova2066@yandex.ru>
2022-10-15 11:17:31 +0300

    add slider edo

--- src/components/hero/hero-contacts/hero-contacts.js
+++ src/components/hero/hero-contacts/hero-contacts.js
@@ -1,38 +1,46 @@
 {
 
-  let myMap;
+  const mapContacts = document.querySelector('#map-contacts');
 
-  ymaps.ready(init);
+  if (mapContacts) {
 
-  function init () {
-    myMap = new ymaps.Map('map-contacts', {
+    let myMap;
 
-      center: [56.13050415063692,37.07479867231817],
-      zoom: 16,
-      controls: []
-    });
+    ymaps.ready(init);
 
-    const pointPickup = [56.130236759852764,37.07856198817317];
-    const pointAddress = [56.130021055835314,37.079999652205565];
+    function init () {
+      myMap = new ymaps.Map('map-contacts', {
 
-    const myPlacemarkPickup = new ymaps.Placemark(pointPickup, {}, {
-      iconLayout: 'default#image',
-      iconImageHref: './assets/img/icons/orange-mark.svg',
-      iconImageSize: [51, 53],
-      iconImageOffset: [0, -53]
-    });
+        center: [56.13050415063692,37.07479867231817],
+        zoom: 16,
+        controls: []
+      });
 
-    const myPlacemarkAddress = new ymaps.Placemark(pointAddress, {}, {
-      iconLayout: 'default#image',
-      iconImageHref: './assets/img/icons/accent-mark.svg',
-      iconImageSize: [51, 53],
-      iconImageOffset: [0, -53]
-    })
+      const pointPickup = [56.130236759852764,37.07856198817317];
+      const pointAddress = [56.130021055835314,37.079999652205565];
 
-    myMap.geoObjects.add(myPlacemarkPickup)
-    myMap.geoObjects.add(myPlacemarkAddress)
+      const myPlacemarkPickup = new ymaps.Placemark(pointPickup, {}, {
+        iconLayout: 'default#image',
+        iconImageHref: './assets/img/icons/orange-mark.svg',
+        iconImageSize: [51, 53],
+        iconImageOffset: [0, -53]
+      });
 
+      const myPlacemarkAddress = new ymaps.Placemark(pointAddress, {}, {
+        iconLayout: 'default#image',
+        iconImageHref: './assets/img/icons/accent-mark.svg',
+        iconImageSize: [51, 53],
+        iconImageOffset: [0, -53]
+      })
+
+      myMap.geoObjects.add(myPlacemarkPickup)
+      myMap.geoObjects.add(myPlacemarkAddress)
+    }
 
   }
+
+
+
+
 
 }
---
+++ src/components/hero/hero-edo/hero-edo.js
@@ -0,0 +1,50 @@
+
+{
+
+  const mobileWidth = window.matchMedia('(max-width: 768px)').matches;
+
+  let mySwiper;
+
+  if (mobileWidth) {
+
+    mySwiper = new Swiper('.hero-point__swiper', {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+
+      uniqueNavElements: true,
+
+      slidesPerView: 1,
+
+      // Бесконечная прокрутка
+      loop: true,
+
+      // Откл функционала, если слайдов меньше, чем нужно
+      watchOverflow: true,
+
+      centeredSlides: true,
+
+      // Отступ между слайдами
+      spaceBetween: 15,
+
+      // Стартовый слайд
+      initialSlide: 0,
+
+      // Брейк поинты (адаптив)
+      // Ширина экрана
+      breakpoints: {
+        // 320: {
+        //   slidesPerView: 1
+        // },
+      }
+    });
+
+  }
+
+
+
+
+
+
+}
